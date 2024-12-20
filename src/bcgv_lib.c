e6cd2833b35ac5aae138827aed50a0ffb140fb3a
Warren Anderson <warren07.anderson@gmail.com>
2024-12-20 11:28:25 +0100

    Advanced next events in fsm and moves bcgv

---
+++ src/bcgv_lib.c
@@ -0,0 +1,583 @@
+#include <stdint.h>
+
+typedef uint8_t cmd_t;
+
+typedef uint8_t frame_num_t;
+
+typedef uint32_t mileage_t;
+
+typedef uint8_t speed_t;
+
+typedef enum 
+ { NO_CHASSIS_PROBLEM = 0, PRESSURE_TIRES = 1, CAR_BRAKE = 2 } chassis_pb_t;
+
+typedef enum 
+ { NO_MOTOR_PROBLEM = 0, PRESSURE_DEFECT = 1, COOLANT = 2, OVERHEATING_OIL = 3 } motor_pb_t;
+
+typedef uint8_t tank_level_t;
+
+typedef uint32_t rpm_t;
+
+typedef enum 
+ { NO_BATTERY_PROBLEM = 0, DISCHARGED = 1, FAILURE = 2 } battery_pb_t;
+
+typedef uint8_t crc_t;
+
+typedef uint8_t id_t;
+
+typedef enum 
+ { OFF = 0, ON = 1 } activation_t;
+
+typedef uint8_t indicator_t;
+
+typedef uint32_t display_mileage_t;
+
+typedef uint32_t display_speed_t;
+
+typedef uint32_t display_tank_level_t;
+
+typedef uint32_t display_rpm_t;
+
+typedef struct {
+    cmd_t cmdWarning;
+    cmd_t cmdPositionLights;
+    cmd_t cmdLowBeams;
+    cmd_t cmdHighBeams;
+    cmd_t cmdRightTurnSignal;
+    cmd_t cmdLeftTurnSignal;
+    cmd_t cmdWindShieldWiper;
+    cmd_t cmdWindShieldWasher;
+    frame_num_t frameNumber;
+    mileage_t mileage;
+    speed_t speed;
+    chassis_pb_t problemChassis;
+    motor_pb_t problemMotor;
+    tank_level_t  tankLevel;
+    rpm_t  rpm;
+    battery_pb_t problemBattery;
+    crc_t crc8;
+    id_t idMsgBcgvToBgf1;
+    id_t idMsgBcgvToBgf2;
+    id_t idMsgBcgvToBgf3;
+    id_t idMsgBcgvToBgf4;
+    id_t idMsgBcgvToBgf5;
+    activation_t activationPositionLights;
+    activation_t activationLowBeams;
+    activation_t activationHighBeams;
+    activation_t activationRightTurnSignal;
+    activation_t activationLeftTurnSignal;
+    indicator_t indicatorPositionLights;
+    indicator_t indicatorLowBeams;
+    indicator_t indicatorHighBeams;
+    indicator_t indicatorFuel;
+    indicator_t indicatorMotorPb;
+    indicator_t indicatorTirePressure;
+    indicator_t indicatorDischargedBattery;
+    indicator_t indicatorWarning;
+    indicator_t indicatorBatteryFailure;
+    indicator_t indicatorTempLDR;
+    indicator_t indicatorMotorPressure;
+    indicator_t indicatorOilOverheating;
+    indicator_t indicatorBrakeFailure;
+    activation_t activationShieldWiper;
+    activation_t activationShieldWasher;
+    display_mileage_t displayMileage;
+    display_speed_t displaySpeed;
+    display_tank_level_t displayTankLevel;
+    display_rpm_t displayRpm;
+} BCGV_Data_t;
+
+static BCGV_Data_t bcgv_data;
+
+cmd_t get_cmdWarning(void) { return bcgv_data.cmdWarning; }
+
+cmd_t get_cmdPositionLights(void) { return bcgv_data.cmdPositionLights; }
+
+cmd_t get_cmdLowBeams(void) { return bcgv_data.cmdLowBeams; }
+
+cmd_t get_cmdHighBeams(void) { return bcgv_data.cmdHighBeams; }
+
+cmd_t get_cmdRightTurnSignal(void) { return bcgv_data.cmdRightTurnSignal; }
+
+cmd_t get_cmdLeftTurnSignal(void) { return bcgv_data.cmdLeftTurnSignal; }
+
+cmd_t get_cmdWindShieldWiper(void) { return bcgv_data.cmdWindShieldWiper; }
+
+cmd_t get_cmdWindShieldWasher(void) { return bcgv_data.cmdWindShieldWasher; }
+
+frame_num_t get_frameNumber(void) { return bcgv_data.frameNumber; }
+
+mileage_t get_mileage(void) { return bcgv_data.mileage; }
+
+speed_t get_speed(void) { return bcgv_data.speed; }
+
+chassis_pb_t get_problemChassis(void) { return bcgv_data.problemChassis; }
+
+motor_pb_t get_problemMotor(void) { return bcgv_data.problemMotor; }
+
+tank_level_t  get_tankLevel(void) { return bcgv_data.tankLevel; }
+
+rpm_t  get_rpm(void) { return bcgv_data.rpm; }
+
+battery_pb_t get_problemBattery(void) { return bcgv_data.problemBattery; }
+
+crc_t get_crc8(void) { return bcgv_data.crc8; }
+
+id_t get_idMsgBcgvToBgf1(void) { return bcgv_data.idMsgBcgvToBgf1; }
+
+id_t get_idMsgBcgvToBgf2(void) { return bcgv_data.idMsgBcgvToBgf2; }
+
+id_t get_idMsgBcgvToBgf3(void) { return bcgv_data.idMsgBcgvToBgf3; }
+
+id_t get_idMsgBcgvToBgf4(void) { return bcgv_data.idMsgBcgvToBgf4; }
+
+id_t get_idMsgBcgvToBgf5(void) { return bcgv_data.idMsgBcgvToBgf5; }
+
+activation_t get_activationPositionLights(void) { return bcgv_data.activationPositionLights; }
+
+activation_t get_activationLowBeams(void) { return bcgv_data.activationLowBeams; }
+
+activation_t get_activationHighBeams(void) { return bcgv_data.activationHighBeams; }
+
+activation_t get_activationRightTurnSignal(void) { return bcgv_data.activationRightTurnSignal; }
+
+activation_t get_activationLeftTurnSignal(void) { return bcgv_data.activationLeftTurnSignal; }
+
+indicator_t get_indicatorPositionLights(void) { return bcgv_data.indicatorPositionLights; }
+
+indicator_t get_indicatorLowBeams(void) { return bcgv_data.indicatorLowBeams; }
+
+indicator_t get_indicatorHighBeams(void) { return bcgv_data.indicatorHighBeams; }
+
+indicator_t get_indicatorFuel(void) { return bcgv_data.indicatorFuel; }
+
+indicator_t get_indicatorMotorPb(void) { return bcgv_data.indicatorMotorPb; }
+
+indicator_t get_indicatorTirePressure(void) { return bcgv_data.indicatorTirePressure; }
+
+indicator_t get_indicatorDischargedBattery(void) { return bcgv_data.indicatorDischargedBattery; }
+
+indicator_t get_indicatorWarning(void) { return bcgv_data.indicatorWarning; }
+
+indicator_t get_indicatorBatteryFailure(void) { return bcgv_data.indicatorBatteryFailure; }
+
+indicator_t get_indicatorTempLDR(void) { return bcgv_data.indicatorTempLDR; }
+
+indicator_t get_indicatorMotorPressure(void) { return bcgv_data.indicatorMotorPressure; }
+
+indicator_t get_indicatorOilOverheating(void) { return bcgv_data.indicatorOilOverheating; }
+
+indicator_t get_indicatorBrakeFailure(void) { return bcgv_data.indicatorBrakeFailure; }
+
+activation_t get_activationShieldWiper(void) { return bcgv_data.activationShieldWiper; }
+
+activation_t get_activationShieldWasher(void) { return bcgv_data.activationShieldWasher; }
+
+display_mileage_t get_displayMileage(void) { return bcgv_data.displayMileage; }
+
+display_speed_t get_displaySpeed(void) { return bcgv_data.displaySpeed; }
+
+display_tank_level_t get_displayTankLevel(void) { return bcgv_data.displayTankLevel; }
+
+display_rpm_t get_displayRpm(void) { return bcgv_data.displayRpm; }
+
+short int set_cmdWarning(cmd_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.cmdWarning = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_cmdPositionLights(cmd_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.cmdPositionLights = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_cmdLowBeams(cmd_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.cmdLowBeams = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_cmdHighBeams(cmd_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.cmdHighBeams = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_cmdRightTurnSignal(cmd_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.cmdRightTurnSignal = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_cmdLeftTurnSignal(cmd_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.cmdLeftTurnSignal = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_cmdWindShieldWiper(cmd_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.cmdWindShieldWiper = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_cmdWindShieldWasher(cmd_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.cmdWindShieldWasher = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_frameNumber(frame_num_t value) {
+    if (value >= 0 && value <= 100) {
+        bcgv_data.frameNumber = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_mileage(mileage_t value) {
+    if (value >= 0) {
+        bcgv_data.mileage = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_speed(speed_t value) {
+    if (value >= 0 && value <= 255) {
+        bcgv_data.speed = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_problemChassis(chassis_pb_t value) {
+    if (value >= 0 && value <= 2) {
+        bcgv_data.problemChassis = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_problemMotor(motor_pb_t value) {
+    if (value >= 0 && value <= 3) {
+        bcgv_data.problemMotor = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_problemBattery(battery_pb_t value) {
+    if (value >= 0 && value <= 2) {
+        bcgv_data.problemBattery = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_crc8(crc_t value) {
+    if (value >= 0 && value <= 255) {
+        bcgv_data.crc8 = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_idMsgBcgvToBgf1(id_t value) {
+    if (value >= 1 && value <= 5) {
+        bcgv_data.idMsgBcgvToBgf1 = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_idMsgBcgvToBgf2(id_t value) {
+    if (value >= 1 && value <= 5) {
+        bcgv_data.idMsgBcgvToBgf2 = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_idMsgBcgvToBgf3(id_t value) {
+    if (value >= 1 && value <= 5) {
+        bcgv_data.idMsgBcgvToBgf3 = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_idMsgBcgvToBgf4(id_t value) {
+    if (value >= 1 && value <= 5) {
+        bcgv_data.idMsgBcgvToBgf4 = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_idMsgBcgvToBgf5(id_t value) {
+    if (value >= 1 && value <= 5) {
+        bcgv_data.idMsgBcgvToBgf5 = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_activationPositionLights(activation_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.activationPositionLights = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_activationLowBeams(activation_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.activationLowBeams = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_activationHighBeams(activation_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.activationHighBeams = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_activationRightTurnSignal(activation_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.activationRightTurnSignal = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_activationLeftTurnSignal(activation_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.activationLeftTurnSignal = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_indicatorPositionLights(indicator_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.indicatorPositionLights = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_indicatorLowBeams(indicator_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.indicatorLowBeams = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_indicatorHighBeams(indicator_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.indicatorHighBeams = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_indicatorFuel(indicator_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.indicatorFuel = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_indicatorMotorPb(indicator_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.indicatorMotorPb = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_indicatorTirePressure(indicator_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.indicatorTirePressure = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_indicatorDischargedBattery(indicator_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.indicatorDischargedBattery = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_indicatorWarning(indicator_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.indicatorWarning = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_indicatorBatteryFailure(indicator_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.indicatorBatteryFailure = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_indicatorTempLDR(indicator_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.indicatorTempLDR = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_indicatorMotorPressure(indicator_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.indicatorMotorPressure = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_indicatorOilOverheating(indicator_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.indicatorOilOverheating = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_indicatorBrakeFailure(indicator_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.indicatorBrakeFailure = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_activationShieldWiper(activation_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.activationShieldWiper = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_activationShieldWasher(activation_t value) {
+    if (value >= 0 && value <= 1) {
+        bcgv_data.activationShieldWasher = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_displayMileage(display_mileage_t value) {
+    if (value >= 0) {
+        bcgv_data.displayMileage = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_displaySpeed(display_speed_t value) {
+    if (value >= 0) {
+        bcgv_data.displaySpeed = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_displayTankLevel(display_tank_level_t value) {
+    if (value >= 0) {
+        bcgv_data.displayTankLevel = value;
+        return 1;
+    }
+    return 0;
+}
+
+short int set_displayRpm(display_rpm_t value) {
+    if (value >= 0 && value <= 1000) {
+        bcgv_data.displayRpm = value;
+        return 1;
+    }
+    return 0;
+}
+
+void init_BCGV_Data(void) {
+    bcgv_data.cmdWarning = 0;
+    bcgv_data.cmdPositionLights = 0;
+    bcgv_data.cmdLowBeams = 0;
+    bcgv_data.cmdHighBeams = 0;
+    bcgv_data.cmdRightTurnSignal = 0;
+    bcgv_data.cmdLeftTurnSignal = 0;
+    bcgv_data.cmdWindShieldWiper = 0;
+    bcgv_data.cmdWindShieldWasher = 0;
+    bcgv_data.frameNumber = 0;
+    bcgv_data.mileage = 0;
+    bcgv_data.speed = 0;
+    bcgv_data.problemChassis = 0;
+    bcgv_data.problemMotor = 0;
+    bcgv_data.tankLevel = 0;
+    bcgv_data.rpm = 0;
+    bcgv_data.problemBattery = 0;
+    bcgv_data.crc8 = 0;
+    bcgv_data.idMsgBcgvToBgf1 = 1;
+    bcgv_data.idMsgBcgvToBgf2 = 2;
+    bcgv_data.idMsgBcgvToBgf3 = 3;
+    bcgv_data.idMsgBcgvToBgf4 = 4;
+    bcgv_data.idMsgBcgvToBgf5 = 5;
+    bcgv_data.activationPositionLights = 0;
+    bcgv_data.activationLowBeams = 0;
+    bcgv_data.activationHighBeams = 0;
+    bcgv_data.activationRightTurnSignal = 0;
+    bcgv_data.activationLeftTurnSignal = 0;
+    bcgv_data.indicatorPositionLights = 0;
+    bcgv_data.indicatorLowBeams = 0;
+    bcgv_data.indicatorHighBeams = 0;
+    bcgv_data.indicatorFuel = 0;
+    bcgv_data.indicatorMotorPb = 0;
+    bcgv_data.indicatorTirePressure = 0;
+    bcgv_data.indicatorDischargedBattery = 0;
+    bcgv_data.indicatorWarning = 0;
+    bcgv_data.indicatorBatteryFailure = 0;
+    bcgv_data.indicatorTempLDR = 0;
+    bcgv_data.indicatorMotorPressure = 0;
+    bcgv_data.indicatorOilOverheating = 0;
+    bcgv_data.indicatorBrakeFailure = 0;
+    bcgv_data.activationShieldWiper = 0;
+    bcgv_data.activationShieldWasher = 0;
+    bcgv_data.displayMileage = 0;
+    bcgv_data.displaySpeed = 0;
+    bcgv_data.displayTankLevel = 0;
+    bcgv_data.displayRpm = 0;
+}
--- src/fsm_turnsignal_warnings.c
+++ src/fsm_turnsignal_warnings.c
@@ -10,6 +10,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <time.h>
+#include "bcgv_lib.c"
 
 /* States */
 typedef enum {
@@ -56,11 +58,14 @@ tTransition trans[] = {
     { ST_OFF, EV_CMD1, &callback2, ST_ACTIVATED_AND_ON},
     { ST_ACTIVATED_AND_ON, EV_ACQ_RECEIVED, &callback2, ST_ACQUITTED},
     { ST_ACTIVATED_AND_ON, EV_ACQ_TIMEOUT, &FsmError, ST_ERROR},
+    { ST_ACTIVATED_AND_ON, EV_CMD1, &FsmError, ST_ACTIVATED_AND_ON},
     { ST_ACTIVATED_AND_ON, EV_CMD0, &callback2, ST_OFF},
     { ST_ACQUITTED, EV_CMD0, &callback1, ST_OFF},
+    { ST_ACQUITTED, EV_CMD1, &callback1, ST_ACQUITTED},
     { ST_ACQUITTED, EV_TIME1, &callback3, ST_ACTIVATED_AND_OFF},
     { ST_ACTIVATED_AND_OFF, EV_ACQ_TIMEOUT, &FsmError, ST_ERROR},
     { ST_ACTIVATED_AND_OFF, EV_ACQ_RECEIVED, &callback3, ST_ACQUITTED},
+    { ST_ACTIVATED_AND_OFF, EV_CMD1, &callback3, ST_ACTIVATED_AND_OFF},
     { ST_ACTIVATED_AND_OFF, EV_CMD0, &callback3, ST_OFF},
     { ST_ACQUITTED, EV_TIME1, &callback3, ST_ACTIVATED_AND_ON},
     { ST_ACQUITTED, EV_CMD0, &callback3, ST_OFF},
@@ -69,11 +74,14 @@ tTransition trans[] = {
 
 #define TRANS_COUNT (sizeof(trans)/sizeof(*trans))
 
-int get_next_event(int current_state)
+int get_next_event(int currentState, unsigned long currentTimeSeconds)
 {
   int event = EV_NONE;
   cmd_t cmd = getCmdPositionLight(); // get the cmd parameter
   activation_t acq = getActivationPositionLight(); // get the acq parameter
+
+  time_t timer = time(NULL);
+  unsigned long timerSeconds = difftime(timer, 0);
 
   if(cmd == 0) {
     event = EV_CMD0;
@@ -86,26 +94,67 @@ int get_next_event(int current_state)
   else if (acq == 0) {
     event = EV_ACQ_TIMEOUT;
   }
+  
+  if(cmd == 0) {
+    event = EV_CMD0;
+  }
+  else if(currentState == ST_ACTIVATED_AND_ON){
+    if(acq == 1){
+      event = EV_ACQ_RECEIVED;
+    }
+    else if((timerSeconds - currentTimeSeconds) > 1){
+      event = EV_ACQ_TIMEOUT;
+    }
+    else { // cmd == 1 
+      event = get_next_event(currentState, currentTimeSeconds);
+    }
+  }
+  else if(currentState == ST_ACTIVATED_AND_OFF){
+    if(acq == 1){
+      event = EV_ACQ_RECEIVED;
+    }
+    else if((timerSeconds - currentTimeSeconds) > 1){
+      event = EV_ACQ_TIMEOUT;
+    }
+    else {
+      event = get_next_event(currentState, currentTimeSeconds);
+    }
+  }
+  else if(currentState == ST_ACQUITTED){
+    if((timerSeconds - currentTimeSeconds) == 1){
+      event = EV_TIME1;
+    }
+    else {
+      event = get_next_event(currentState, currentTimeSeconds);
+    }
+  }
 
 
-
-
+  else if(cmd == 1){
+    event = EV_CMD1;
+  }
+  else {
+    event = EV_ERR;
+  }
   return event;
 }
 
 
 int main(void)
-{
+{ 
     int i = 0;
     int ret = 0; 
     int event = EV_NONE;
     int state = ST_INIT;
+
+    time_t currentTime = time(NULL);
+    unsigned long currentTimeSeconds = difftime(currentTime, 0);
     
     /* While FSM hasn't reach end state */
     while (state != ST_TERM) {
         
         /* Get event */
-        event = get_next_event(state);
+        event = get_next_event(state, currentTimeSeconds);
         
         /* For each transitions */
         for (i = 0; i < TRANS_COUNT; i++) {
