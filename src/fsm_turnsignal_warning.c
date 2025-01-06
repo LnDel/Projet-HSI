9cfc56160cf67c6d85b05e83d3206618cc8c7740
Warren Anderson <warren07.anderson@gmail.com>
2025-01-06 20:01:46 +0100

    started to modidy events and states

--- src/fsm_turnsignal_warning.c
+++ src/fsm_turnsignal_warning.c
@@ -23,7 +23,7 @@ typedef enum {
   ST_ACTIVATED_AND_OFF= 4,
   ST_ERROR=5,
   ST_TERM = 255,                 /* Final state */
-} fsm_state_t;
+} turn_signal_state_t;
 
 /* Events */
 typedef enum {
@@ -35,18 +35,30 @@ typedef enum {
   EV_ACQ_TIMEOUT= 4,
   EV_TIME1=5,
   EV_ERR = 255                            /* Error event */
-} fsm_event_t;
+} turn_signal_event_t;
 
 /* Callback functions called on transitions */
-static int callback1 (void) { printf("callback1"); };
+static int callback1 (void) { //ST_OFF
+  printf("callback1"); 
+};
+static int callback2 (void) { //ST_ACTIVATED_AND_ON
+  printf("callback1"); 
+};
+static int callback3 (void) { //ST_ACQUITTED
+  printf("callback1"); 
+};
+static int callback4 (void) { //ST_ACTIVATED_AND_OFF
+  printf("callback1"); 
+};
+
 static int callback2 (void) { printf("callback2");};
 static int callback3 (void) { printf("callback3"); };
 static int FsmError(void) {   printf("ERREUR");};
 
 /* Transition structure */
 typedef struct {
-  fsm_state_t state;
-  fsm_event_t event;
+  turn_signal_state_t state;
+  turn_signal_event_t event;
   int (*callback)(void);
   int next_state;
 } tTransition;
@@ -56,29 +68,29 @@ tTransition trans[] = {
   /* These are examples */
   { ST_INIT, EV_ANY, &callback1, ST_OFF},
   { ST_OFF, EV_CMD1, &callback2, ST_ACTIVATED_AND_ON},
-  { ST_ACTIVATED_AND_ON, EV_ACQ_RECEIVED, &callback2, ST_ACQUITTED},
+  { ST_ACTIVATED_AND_ON, EV_ACQ_RECEIVED, &callback3, ST_ACQUITTED},
   { ST_ACTIVATED_AND_ON, EV_ACQ_TIMEOUT, &FsmError, ST_ERROR},
-  { ST_ACTIVATED_AND_ON, EV_CMD1, &FsmError, ST_ACTIVATED_AND_ON},
-  { ST_ACTIVATED_AND_ON, EV_CMD0, &callback2, ST_OFF},
+  { ST_ACTIVATED_AND_ON, EV_CMD1, &callback2, ST_ACTIVATED_AND_ON},
+  { ST_ACTIVATED_AND_ON, EV_CMD0, &callback1, ST_OFF},
   { ST_ACQUITTED, EV_CMD0, &callback1, ST_OFF},
-  { ST_ACQUITTED, EV_CMD1, &callback1, ST_ACQUITTED},
-  { ST_ACQUITTED, EV_TIME1, &callback3, ST_ACTIVATED_AND_OFF},
+  { ST_ACQUITTED, EV_CMD1, &callback3, ST_ACQUITTED},
+  { ST_ACQUITTED, EV_TIME1, &callback4, ST_ACTIVATED_AND_OFF},
   { ST_ACTIVATED_AND_OFF, EV_ACQ_TIMEOUT, &FsmError, ST_ERROR},
   { ST_ACTIVATED_AND_OFF, EV_ACQ_RECEIVED, &callback3, ST_ACQUITTED},
-  { ST_ACTIVATED_AND_OFF, EV_CMD1, &callback3, ST_ACTIVATED_AND_OFF},
-  { ST_ACTIVATED_AND_OFF, EV_CMD0, &callback3, ST_OFF},
-  { ST_ACQUITTED, EV_TIME1, &callback3, ST_ACTIVATED_AND_ON},
-  { ST_ACQUITTED, EV_CMD0, &callback3, ST_OFF},
+  { ST_ACTIVATED_AND_OFF, EV_CMD1, &callback4, ST_ACTIVATED_AND_OFF},
+  { ST_ACTIVATED_AND_OFF, EV_CMD0, &callback1, ST_OFF},
+  { ST_ACQUITTED, EV_TIME1, &callback2, ST_ACTIVATED_AND_ON},
+  { ST_ACQUITTED, EV_CMD0, &callback1, ST_OFF},
   { ST_ANY, EV_ERR, &FsmError, ST_TERM}
 };
 
 #define TRANS_COUNT (sizeof(trans)/sizeof(*trans))
 
-int get_next_event(int currentState, unsigned long currentTimeSeconds)
+turn_signal_event_t get_next_event(int currentState, unsigned long currentTimeSeconds)
 {
-  int event = EV_NONE;
+  turn_signal_event_t event = EV_NONE;
   cmd_t cmd = getCmdPositionLight(); // get the cmd parameter
-  activation_t acq = getActivationPositionLight(); // get the acq parameter
+  indicator_t acq = getIndicatorPositionLight(); // get the acq parameter
 
   time_t timer = time(NULL);
   unsigned long timerSeconds = difftime(timer, 0);
@@ -139,24 +151,17 @@ int get_next_event(int currentState, unsigned long currentTimeSeconds)
 }
 
 
-int main(void)
+turn_signal_state_t main(turn_signal_state_t currentState)
 { 
-  int i = 0;
   int ret = 0; 
-  int event = EV_NONE;
-  int state = ST_INIT;
+  turn_signal_state_t state = currentState;
+  turn_signal_event_t event = get_next_event(state, time(NULL)); 
 
   time_t currentTime = time(NULL);
   unsigned long currentTimeSeconds = difftime(currentTime, 0);
-  
-  /* While FSM hasn't reach end state */
-  while (state != ST_TERM) {
-    
-    /* Get event */
-    event = get_next_event(state, currentTimeSeconds);
-    
+
     /* For each transitions */
-    for (i = 0; i < TRANS_COUNT; i++) {
+    for (int i = 0; i < TRANS_COUNT; i++) {
       /* If State is current state OR The transition applies to all states ...*/
       if ((state == trans[i].state) || (ST_ANY == trans[i].state)) {
         /* If event is the transition event OR the event applies to all */
@@ -171,7 +176,6 @@ int main(void)
         }
       }
     }
-  }
 
   return ret;
 }
