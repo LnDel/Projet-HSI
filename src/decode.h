7a78b498bc66d0864c66eb838d3cc0d18ecf300d
Helene Delran <delrangarr@cy-tech.fr>
2025-01-09 20:46:11 +0100

    add decode serial frame

--- src/decode.h
+++ src/decode.h
@@ -1,3 +1,10 @@
+/**
+ * \file    decode.h
+ * \brief   Decoding functions for COMODO and MUX frames to BCGV
+ * \details This file contains declarations for functions related to decoding frames received
+ * \author  Hélène Delran-Garric
+ */
+
 #ifndef DECODE_FRAMES_H
 #define DECODE_FRAMES_H
 
@@ -5,17 +12,16 @@
 #include "bcgv_lib.h"
 
 /**
- * @brief Decodes a COMODO -> BCGV frame and updates the corresponding application data.
- * 
- * @param frame A single byte representing the COMODO frame.
+ * \brief Decodes the COMODO -> BCGV frame and updates the corresponding application data.
+ * \param[in] frame A pointer to the COMODO frame.
  */
-void decode_comodo_to_bcgv(serial_frame_t);
+void decode_comodo_to_bcgv(uint8_t* frame);
 
 /**
- * @brief Decodes a MUX -> BCGV frame and updates the corresponding application data.
+ * \brief Decodes a MUX -> BCGV frame and updates the corresponding application data.
  *        Ensures that the frame's CRC8 is valid before decoding.
- * 
- * @param frame An array of 15 bytes representing the MUX frame.
+ * \param[in] frame A pointer to the MUX frame.
+*  \return is_valid_frame_t Returns VALID if the frame is valid (correct CRC), otherwise INVALID.
  */
 is_valid_frame_t decode_mux_to_bcgv(uint8_t* frame);
 
